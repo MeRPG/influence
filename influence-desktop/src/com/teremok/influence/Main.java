39528a720fe43d248620cbf3b78cf88fd4d639e3
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2013-12-11 01:04:02 +0300

    initial commit

---
+++ influence-desktop/src/com/teremok/influence/Main.java
@@ -0,0 +1,16 @@
+package com.teremok.influence;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "influence";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
