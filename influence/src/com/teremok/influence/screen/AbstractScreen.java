089cc89e02469dc3e39e88efccb94f46ec3e23e1
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2013-12-23 00:28:18 +0300

    add abstract screen to move on scene2d based architecture in future

---
+++ influence/src/com/teremok/influence/screen/AbstractScreen.java
@@ -0,0 +1,102 @@
+package com.teremok.influence.screen;
+
+/**
+ * Created by Alexx on 22.12.13
+ * */
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+/**
+ * The base class for all game screens.
+ */
+public abstract class AbstractScreen implements Screen {
+    protected final Game game;
+    protected BitmapFont font;
+    protected SpriteBatch batch;
+    protected Skin skin;
+    protected final Stage stage;
+
+    public AbstractScreen(Game game) {
+        this.game = game;
+        this.stage = new Stage( 0, 0, true );
+    }
+
+    public BitmapFont getFont() {
+        if (font == null)
+            font = new BitmapFont();
+        return font;
+    }
+
+    public SpriteBatch getBatch() {
+        if (batch == null)
+            batch = new SpriteBatch();
+        return batch;
+    }
+
+    public Skin getSkin() {
+        if( skin == null ) {
+            skin = new Skin( Gdx.files.internal( "uiskin.json" ), new TextureAtlas(Gdx.files.internal( "uiskin.png" )) );
+        }
+        return skin;
+    }
+
+    protected String getName()
+    {
+        return getClass().getSimpleName();
+    }
+
+    // Screen implementation
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor( stage );
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // resize the stage
+        stage.setViewport(width, height, true);
+    }
+
+    @Override
+    public void render(float delta) {
+
+        stage.act( delta );
+
+        // the following code clears the screen with the given RGB color (black)
+        Gdx.gl.glClearColor( 0f, 0f, 0f, 1f );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT );
+
+        // update and draw the stage actors
+        stage.draw();
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+        // dispose the collaborators
+        if (stage != null) stage.dispose();
+        if (batch != null) batch.dispose();
+        if (font  != null) font.dispose();
+    }
+}
