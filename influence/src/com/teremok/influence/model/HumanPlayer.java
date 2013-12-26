d005fb7998b133c2cf88b1bdf3ff5b7bd272ea64
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2013-12-26 11:58:48 +0300

    add player abstractions

---
+++ influence/src/com/teremok/influence/model/HumanPlayer.java
@@ -0,0 +1,10 @@
+package com.teremok.influence.model;
+
+/**
+ * Created by Alexx on 26.12.13
+ */
+public class HumanPlayer extends Player {
+    public HumanPlayer(int type) {
+        super(type);
+    }
+}
---
+++ influence/src/com/teremok/influence/model/Player.java
@@ -0,0 +1,58 @@
+package com.teremok.influence.model;
+
+/**
+ * Created by Alexx on 26.12.13
+ */
+public class Player {
+    static private int currentNum = 0;
+    static private Player[] players = new Player[5];
+
+    protected int type;
+    protected int power;
+
+    static public void addPlayer(Player player, int num) {
+        if (num < 0 || num > 4) {
+            return;
+        }
+
+        players[num] = player;
+
+    }
+
+    static public Player next() {
+        currentNum++;
+        if (currentNum > 4) {
+            currentNum = 0;
+        }
+        System.out.println("Turn ended. Next player : " + currentNum);
+        return players[currentNum];
+    }
+
+    static public Player current() {
+        return players[currentNum];
+    }
+
+    // Non-static
+
+    public Player(int type) {
+        this.type = type;
+    }
+
+    public void setPowerToDistribute(int power) {
+        this.power = power;
+    }
+
+    public int getPowerToDistribute() {
+        return power;
+    }
+
+    public void subtractPowerToDistribute() {
+        power--;
+    }
+
+    // Auto-generated
+
+    public int getType() {
+        return type;
+    }
+}
