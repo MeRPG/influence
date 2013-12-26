42d14e3420b6c6643aed5f208c6719305f88f13e
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2013-12-26 12:00:27 +0300

    add current player drawing and power on distribute phase

--- influence/src/com/teremok/influence/view/ScoreDrawer.java
+++ influence/src/com/teremok/influence/view/ScoreDrawer.java
@@ -1,7 +1,9 @@
 package com.teremok.influence.view;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.teremok.influence.model.Player;
 import com.teremok.influence.model.Score;
+import com.teremok.influence.screen.GameScreen;
 import com.teremok.influence.util.DrawHelper;
 
 /**
@@ -19,14 +21,28 @@ public class ScoreDrawer extends AbstractDrawer<Score> {
 
         batch.begin();
         drawScores(batch);
+        drawPower(batch);
     }
 
     private void drawScores(SpriteBatch batch) {
         int i = 0;
         for (int score : current.getScores()) {
             bitmapFont.setColor(DrawHelper.getCellColorByType(i));
-            bitmapFont.draw(batch, score + " ", 25f + i*50f, current.getHeight()/2);
+            String toDraw;
+            if (i == Player.current().getType()) {
+                toDraw = "-" + score + "-";
+            } else {
+                toDraw = " " + score + " ";
+            }
+            bitmapFont.draw(batch, toDraw, 25f + i*50f, current.getHeight()/2);
             i++;
+        }
+    }
+
+    private void drawPower(SpriteBatch batch) {
+        if (GameScreen.currentPhase == GameScreen.TurnPhase.DISTRIBUTE) {
+            bitmapFont.setColor(DrawHelper.getCellColorByType(Player.current().getType()));
+            bitmapFont.draw(batch, "(" +Player.current().getPowerToDistribute() + ") ", current.getWidth() - 25, current.getHeight()/2);
         }
     }
 }
