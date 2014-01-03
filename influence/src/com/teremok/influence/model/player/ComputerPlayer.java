e90af4fd9fc54ab33d7236dd0ee9a7c8bf1848ba
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2014-01-03 10:15:26 +0300

    simplified computer player logic

--- influence/src/com/teremok/influence/model/player/ComputerPlayer.java
+++ influence/src/com/teremok/influence/model/player/ComputerPlayer.java
@@ -7,17 +7,24 @@ import com.teremok.influence.screen.GameScreen;
 import java.util.*;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Alexx
- * Date: 29.12.13
- * Time: 17:00
- * Email: alexey.gorovoy.work@gmail.com
+ * Created by Alexx on 29.12.13
  */
 public class ComputerPlayer extends Player {
 
-    public final static float TURN_DELAY = .50f;
-    private Map<Integer, Cell> actions;
+    public final static float TURN_DELAY = .5f;
+    private Move nextMove;
     private Random rnd = new Random();
+    private float turnTime;
+
+    private class Move {
+        private Cell cell;
+        private Cell enemy;
+
+        private Move(Cell cell, Cell enemy) {
+            this.cell = cell;
+            this.enemy = enemy;
+        }
+    }
 
     public ComputerPlayer(int type, GameScreen screen, Field field) {
         super(type, screen, field);
@@ -25,78 +32,73 @@ public class ComputerPlayer extends Player {
 
     protected void actLogic(float delta) {
         if (gameScreen.currentPhase == GameScreen.TurnPhase.ATTACK) {
+            System.out.println("actAttackLogic, time = " + turnTime);
             actAttackLogic(delta);
         } else {
+            System.out.println("actDistributeLogic, time = " + turnTime);
             actDistributeLogic(delta);
         }
     }
 
-    private float turnTime = 0;
-    private int turn = 0;
-    boolean oneMoreMove;
-
     private void actAttackLogic(float delta) {
 
-        if (turnTime == 0 || oneMoreMove) {
+        if (nextMove == null) {
+            System.out.println("prepareActions, time = " + turnTime);
             prepareActions();
 
+            turnTime = 0;
+            if (nextMove == null) {
+                System.out.println("setDistributePhase, time = " + turnTime);
+                gameScreen.setDistributePhase();
+            }
+
         } else {
-            if (turnTime / TURN_DELAY > turn ) {
-                doFirstAction();
+            if (turnTime > TURN_DELAY) {
+                System.out.println("doNextMove, time = " + turnTime);
+                doNextMove();
+                turnTime = 0;
             }
         }
         turnTime += delta;
     }
 
     private void prepareActions() {
-        int t = 0;
-        actions = new HashMap<Integer, Cell>();
-        turn = 0;
-        oneMoreMove = false;
         for (Cell cell : field.getCells()) {
             if (cell.isValid() && cell.getType() == type && cell.getPower() > 1) {
                 List<Cell> enemies = field.getConnectedEnemies(cell);
                 if (! enemies.isEmpty()) {
                     int cellNumberToAttack = rnd.nextInt(enemies.size());
-                    Cell cellToAttack = enemies.get(cellNumberToAttack);
-                    actions.put(t++, cell);
-                    actions.put(t++, cellToAttack);
+                    Cell enemy = enemies.get(cellNumberToAttack);
+                    nextMove = new Move(cell, enemy);
+                    break;
                 }
             }
         }
     }
 
-    private void doFirstAction() {
-        Cell cell = actions.get(turn);
-        if (! actions.isEmpty() && cell != null){
-            cell = actions.get(turn);
-
-            int beforeType = cell.getType();
-            System.out.println("Selecting cell: " + cell);
-            field.setSelectedCell(cell);
-
-            actions.remove(turn);
-
-            if (cell.getType() != beforeType && cell.getPower() > 1) {
-                oneMoreMove = true;
-                turnTime = 0;
-            }
-
-            turn++;
+    private void doNextMove() {
+        Cell cell;
+        if (nextMove.cell != null) {
+            cell = nextMove.cell;
+            nextMove.cell = null;
         } else {
-            turn = 0;
-            turnTime = 0;
-            gameScreen.setDistributePhase();
+            cell = nextMove.enemy;
+            nextMove = null;
         }
+
+        System.out.println("Selecting cell: " + cell);
+        field.setSelectedCell(cell);
     }
 
     private void actDistributeLogic(float delta) {
-        System.out.println("Distributing power!");
-        for (Cell cell : field.getCells()) {
-            if (cell.isValid() && cell.getType() == type && powerToDistribute > 0) {
-                field.addPower(cell);
+        if (turnTime > TURN_DELAY) {
+            System.out.println("Distributing power!");
+            for (Cell cell : field.getCells()) {
+                if (cell.isValid() && cell.getType() == type && powerToDistribute > 0) {
+                    field.addPower(cell);
+                }
             }
         }
-        oneMoreMove = true;
+        turnTime += delta;
     }
 }
