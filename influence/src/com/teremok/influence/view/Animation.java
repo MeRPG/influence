e80bca958481312d01dd2913487620ded00d674f
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2014-01-09 23:21:19 +0300

    add animation duration constants

---
+++ influence/src/com/teremok/influence/view/Animation.java
@@ -0,0 +1,10 @@
+package com.teremok.influence.view;
+
+/**
+ * Created by Alexx on 09.01.14
+ */
+public interface Animation {
+    static float DURATION_SHORT = 0.25f;
+    static float DURATION_NORMAL = 0.75f;
+    static float DURATION_LONG = 1.5f;
+}
