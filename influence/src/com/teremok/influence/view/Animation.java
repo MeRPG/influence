f3774a71d39211c0643831c09a9d249666300299
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2014-01-13 11:28:21 +0300

    animations now faster

--- influence/src/com/teremok/influence/view/Animation.java
+++ influence/src/com/teremok/influence/view/Animation.java
@@ -5,6 +5,6 @@ package com.teremok.influence.view;
  */
 public interface Animation {
     static float DURATION_SHORT = 0.25f;
-    static float DURATION_NORMAL = 0.75f;
+    static float DURATION_NORMAL = 0.5f;
     static float DURATION_LONG = 1.5f;
 }
