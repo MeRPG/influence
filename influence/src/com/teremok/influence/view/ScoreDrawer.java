7e01af95b42f772669f95c6e6f9d25adc1210c62
Alexey Gorovoy <alexey.gorovoy.work@gmail.com>
2014-01-25 01:25:45 +0300

    fix tips

--- influence/src/com/teremok/influence/view/ScoreDrawer.java
+++ influence/src/com/teremok/influence/view/ScoreDrawer.java
@@ -1,32 +1,19 @@
 package com.teremok.influence.view;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.teremok.influence.model.Cell;
 import com.teremok.influence.model.Localizator;
-import com.teremok.influence.model.player.PlayerManager;
-import com.teremok.influence.model.player.Player;
 import com.teremok.influence.model.Score;
-import com.teremok.influence.screen.AbstractScreen;
 
-import static com.teremok.influence.view.Drawer.UNIT_SIZE;
 import static com.teremok.influence.view.Drawer.getCellColorByType;
 
 /**
  * Created by Alexx on 24.12.13
  */
 public class ScoreDrawer extends AbstractDrawer<Score> {
-
-    private static float LEFT_MARGIN = UNIT_SIZE * 0.8f;
-    private static float RIGHT_MARGIN = UNIT_SIZE * 1.1f;
-    private static float TEXT_PADDING = UNIT_SIZE * 1.6f;
-
-
-    float[] newWidth = new float[5];
 
     @Override
     public void draw(Score score, SpriteBatch batch, float parentAlpha) {
@@ -53,19 +40,9 @@ public class ScoreDrawer extends AbstractDrawer<Score> {
         renderer.end();
     }
 
-    private void drawPower(SpriteBatch batch) {
-        PlayerManager pm = current.getPm();
-        if (current.getMatch().isInDistributePhase()) {
-            bitmapFont.setColor(Drawer.getCellColorByType(pm.current().getType()));
-            String text = "(" +pm.current().getPowerToDistribute() + ") ";
-            bitmapFont.draw(batch, text, current.getWidth() - RIGHT_MARGIN, current.getHeight()/2);
-        }
-    }
-
     private void drawStatusString(SpriteBatch batch) {
 
         String colorString = Localizator.getString("ofYourColor");
-
 
         BitmapFont.TextBounds bounds = bitmapFont.getBounds(current.getStatus());
 
@@ -97,8 +74,6 @@ public class ScoreDrawer extends AbstractDrawer<Score> {
             bitmapFont.setColor(getCellColorByType(current.getPm().current().getType()));
             bitmapFont.draw(batch, colorString, x + X, y);
             bitmapFont.draw(batch, powerString, x + X + Y, y);
-
-
 
         } else {
 
