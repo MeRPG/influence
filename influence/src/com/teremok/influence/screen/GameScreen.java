e8c51846e793c9ddb0d7d70b0b2b95947cfd56af
Alexey <alexey.gorovoy.work@gmail.com>
2014-04-12 23:27:48 +0300

    Новые текстуры для стартового экрана

--- influence/src/com/teremok/influence/screen/GameScreen.java
+++ influence/src/com/teremok/influence/screen/GameScreen.java
@@ -25,7 +25,6 @@ public class GameScreen extends StaticScreen {
     Match match;
     PausePanel pausePanel;
     TexturePanel backlight;
-    TexturePanel backdark;
 
     long lastBackPress = 0;
 
@@ -80,9 +79,6 @@ public class GameScreen extends StaticScreen {
         stage.getRoot().clearChildren();
         stage.addActor(backlight);
         stage.addActor(match.getField());
-        if (! (Settings.gameSettings.fieldSize == FieldSize.SMALL
-                || Settings.gameSettings.fieldSize == FieldSize.NORMAL))
-            stage.addActor(backdark);
         stage.addActor(match.getScore());
         stage.addActor(TooltipHandler.getInstance());
         stage.addActor(pausePanel);
@@ -169,7 +165,6 @@ public class GameScreen extends StaticScreen {
     protected void addActors() {
         AbstractDrawer.setBitmapFont(getFont());
         initBacklight();
-        backdark = new TexturePanel(uiElements.get("backdark"));
         pausePanel = new PausePanel(this);
         updateMatchDependentActors();
     }
