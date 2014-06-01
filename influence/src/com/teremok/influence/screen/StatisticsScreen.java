8052921708a396795003c9f575ceab0807a3db90
Alexey <alexey.gorovoy.work@gmail.com>
2014-06-02 00:11:04 +0300

    первые попытки отправки http запросов серверу

--- influence/src/com/teremok/influence/screen/StatisticsScreen.java
+++ influence/src/com/teremok/influence/screen/StatisticsScreen.java
@@ -1,12 +1,19 @@
 package com.teremok.influence.screen;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.HttpParametersUtils;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.teremok.influence.model.Chronicle;
 import com.teremok.influence.ui.TextureNumber;
+import com.teremok.influence.util.Logger;
 import com.teremok.influence.util.TextureNumberFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Алексей on 26.05.2014
@@ -59,6 +66,51 @@ public class StatisticsScreen extends StaticScreen {
                     return true;
                 }
                 return false;
+            }
+
+
+        });
+        stage.addListener( new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                if (! event.isHandled() && isInfoMessageVisible()) {
+                    hideInfoMessageAnimation();
+                    event.handle();
+                    return;
+                }
+                if (! event.isHandled()) {
+
+                    event.handle();
+                    Map<String, String> parameters = new HashMap<>();
+                    parameters.put("id", 1 +"");
+                    parameters.put("influence", Chronicle.influence +"");
+
+                    Net.HttpRequest httpGet = new Net.HttpRequest(Net.HttpMethods.GET);
+                    httpGet.setUrl("http://timeforlime.ru/influence/index.php");
+                    httpGet.setContent(HttpParametersUtils.convertHttpParameters(parameters));
+
+                    Gdx.net.sendHttpRequest (httpGet, new Net.HttpResponseListener() {
+                        public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                            String status = httpResponse.getResultAsString();
+                            int stat = Integer.parseInt(status);
+                            addInfoMessage(new TextureNumberFactory().getNumber(stat, 100, 100, false));
+                            showInfoMessageAnimation();
+                            Logger.log("HttpResponse " + status);
+                        }
+
+                        public void failed(Throwable t) {
+                            String status = "failed";
+                            //do stuff here based on the failed attempt
+                            Logger.log("HttpResponse " + status);
+                            t.printStackTrace();
+                        }
+                    });
+                }
             }
         });
     }
