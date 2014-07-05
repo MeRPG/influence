cf7e74f2a6bf7729b045eeaa03c87c14091e82de
Alexey <alexey.gorovoy.work@gmail.com>
2014-07-05 22:36:10 +0300

    Изменен цвет экрана загрузки

--- influence/src/com/teremok/influence/screen/SplashScreen.java
+++ influence/src/com/teremok/influence/screen/SplashScreen.java
@@ -2,6 +2,7 @@ package com.teremok.influence.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.teremok.influence.Influence;
 import com.teremok.influence.util.ResourceManager;
@@ -13,11 +14,13 @@ public class SplashScreen extends AbstractScreen {
 
     ShapeRenderer renderer;
     ResourceManager resourceManager;
+    Color color = new Color(0x35b7deff);
 
     public SplashScreen(Influence game) {
         super(game);
         resourceManager = game.getResourceManager();
         renderer = new ShapeRenderer();
+        renderer.setColor(color);
     }
 
     @Override
@@ -31,21 +34,23 @@ public class SplashScreen extends AbstractScreen {
             game.getScreenController().showStartScreen();
         }
 
+        Gdx.gl.glLineWidth(2);
+
         renderer.setTransformMatrix(getBatch().getTransformMatrix());
         renderer.setProjectionMatrix(getBatch().getProjectionMatrix());
 
-        renderer.setColor(Color.WHITE);
         renderer.begin(ShapeRenderer.ShapeType.Line);
-        renderer.rect(180, 347, 120, 26);
+        renderer.rect(122, 347, 236, 27);
         renderer.end();
-        int border = 3;
+        int border = 5;
         float progress = resourceManager.getAssetManager().getProgress();
-        renderer.setColor(Color.WHITE);
+
         renderer.begin(ShapeRenderer.ShapeType.Filled);
-        renderer.rect(180+border,347+border, (120-border*2)*progress, 26-border*2);
+        renderer.rect(122+border,347+border, (236-border*2)*progress, 27-border*2);
         renderer.end();
 
 
+        Gdx.gl.glLineWidth(1);
 
     }
 }
